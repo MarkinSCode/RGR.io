617dbe79a9a9e63464bdd12a563adc2bb4a568ab
MarkinSCode <65231865+MarkinSCode@users.noreply.github.com>
2023-05-14 23:57:38 +0400

    Add files via upload

--- js/app.js
+++ js/app.js
@@ -5,7 +5,7 @@ function slowScroll(id) {
      }
      
      $(document).on("scroll", function () {
-      if($(window).scrollTop() === 0)
+      if($(window).scrollTop() == 0)
       $("header").removeClass("fixed");
       else
       $("header").attr("class", "fixed");
