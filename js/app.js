8ed328f70477527c1315e7e369c2581f2905f8d9
MarkinSCode <65231865+MarkinSCode@users.noreply.github.com>
2023-05-24 08:10:48 +0400

    Add files via upload

--- js/app.js
+++ js/app.js
@@ -4,9 +4,14 @@ function slowScroll(id) {
      }, 500);
      }
      
-     $(document).on("scroll", function () {
-      if($(window).scrollTop() == 0)
-      $("header").removeClass("fixed");
-      else
-      $("header").attr("class", "fixed");
-     });
+$(document).on("scroll", function () {
+     if($(window).scrollTop() == 0)
+     $("header").removeClass("fixed");
+     else
+     $("header").attr("class", "fixed");
+});
+$(window).on('load', function() {
+	$(".loader").fadeOut(); 
+	$("#preloder").delay(400).fadeOut("slow");
+
+});
